7ce7194a5210c2bf04a8cebaece936f73a7c0848
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2024-12-07 23:37:14 +0530

    first commit

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,18 @@
+import { Server } from 'socket.io';
+
+
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log('SocketService constructor');
+    this._io = new Server();
+  }
+
+  get io(){
+    return this._io;
+  }
+}
+
+export default SocketService;
