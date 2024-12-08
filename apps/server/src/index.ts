4a97b998f8e449788b843cfeda79dfb7a00babb4
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2024-12-08 17:39:07 +0530

    Configure Redis: Add environment variables for host and port

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -15,6 +15,8 @@ async function init(){
     httpServer.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
+
+    socketService.initListeners();
 }
 
 init();
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,13 +1,62 @@
 import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+import Redis from 'ioredis';
+dotenv.config();
 
+const pub = new Redis({
+  host : process.env.REDIS_HOST||'localhost',
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+  // username: 'root',
+  // password: 'root'
+});
+const sub = new Redis({
+  host : process.env.REDIS_HOST||'localhost',
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+  // username: 'root',
+  // password: 'root'
 
+});
 
 class SocketService {
   private _io: Server;
 
   constructor() {
     console.log('SocketService constructor');
-    this._io = new Server();
+    this._io = new Server({
+        cors: {
+            origin: '*',
+            allowedHeaders: ['*']
+        }
+    });
+    sub.subscribe('MESSAGES');
+  }
+
+  public initListeners() {
+    const io = this.io;
+    console.log('SocketService initListeners');
+    io.on('connect', (socket) => {
+      console.log('a user connected' , socket.id);
+
+      socket.on('event:message', async ({message}:{message:string})=>{
+        console.log("New message: ", message);
+
+        // Publish the message to Redis
+        await pub.publish('MESSAGES', JSON.stringify({message}));
+      });
+
+
+    //   socket.on('disconnect', () => {
+    //     console.log('user disconnected');
+    //   });
+    });
+    sub.on('message', (channel, message) => {
+      console.log('Received message: ', message);
+      if (channel === 'MESSAGES'){
+        // io.emit('event:message', JSON.parse(message));
+        io.emit('message', message);
+      }
+     
+    });
   }
 
   get io(){
